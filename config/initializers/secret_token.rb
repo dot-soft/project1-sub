4591d53450c695eb6051f60ad8f40969c94d2b81
dot-soft <dot-soft@mail.com>
2013-01-12 18:45:21 +0330

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webashpaz::Application.config.secret_token = '275b75186728816fb04ea8d3226e590e4a3c6b1a3065d6eed2ad9ac456eff499c43e74b8bc103a1774a120eda57dcd0db95240c0de1df612f0d3cf511622fc33'
